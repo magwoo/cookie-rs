27bde1878e747a42b9981d7d15fb6ae292bf3810
magwoo <magwood51@yandex.ru>
2024-08-03 02:37:32 +0300

    change root pub exports and added prelude

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
+pub use crate::cookie::builder::CookieBuilder;
 pub use crate::cookie::Cookie;
 pub use crate::jar::CookieJar;
 
 mod cookie;
 mod jar;
+
+pub mod prelude {
+    pub use crate::cookie::SameSite;
+    pub use crate::*;
+}
